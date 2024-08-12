bf796182753c0affc3183607704e6a0d2fbd7854
Anton Zaitsev <zaitsev.anton@outlook.com>
2024-08-13 00:35:47 +0200

    chore(frontend): cleanup

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
-import NotFoundView from '@/views/NotFoundView.vue';
 import HomeView from '@/views/HomeView.vue';
 import LoginView from '@/views/LoginView.vue';
 import POIsView from '@/views/POIsView.vue';
--- frontend/src/views/NotFoundView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div><h1>Not Found</h1></div>
-</template>
-
-<style scoped></style>
