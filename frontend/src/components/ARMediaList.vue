ba38cff1db77bd539edd00a348ea7e1d80507496
Anton Zaitsev <zaitsev.anton@outlook.com>
2024-08-16 02:10:20 +0200

    feat(frontend): add ar-media for a route

--- frontend/src/components/ARMediaList.vue
+++ frontend/src/components/ARMediaList.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
-import { PropType } from 'vue';
-import { ARMedia } from '@/api/arMedia';
+import { PropType, ref } from 'vue';
+import { addARMedia, deleteARMedia, ARMedia } from '@/api/arMedia';
+import { useRoute } from 'vue-router';
+import { useToast } from 'vue-toastification';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+
+const toast = useToast();
+
+const route = useRoute();
+const routeId = route.params.routeId as string;
+const loading = ref(false);
+const deletingMedia = ref<string | null>(null);
+
+const githubOwner = import.meta.env.VITE_GH_OWNER as string;
+const githubRepo = import.meta.env.VITE_GH_REPO as string;
 
 defineProps({
   arMedia: {
@@ -8,16 +21,108 @@ defineProps({
     required: true,
   },
 });
+
+const handleARMediaUpload = async (event: Event) => {
+  loading.value = true;
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files[0]) {
+    try {
+      await addARMedia(routeId, input.files[0]);
+      toast.success('Änderung erfolgreich gespeichert');
+    } catch (error) {
+      console.error('Error uploading AR media:', error);
+      toast.error('Änderung konnte nicht gespeichert werden');
+    } finally {
+      loading.value = false;
+    }
+  }
+};
+
+const handleARMediaDelete = async (media: ARMedia) => {
+  loading.value = true;
+  try {
+    await deleteARMedia(routeId, media.id);
+    toast.success('Änderung erfolgreich gespeichert');
+  } catch (error) {
+    console.error('Error deleting AR media:', error);
+    toast.error('Änderung konnte nicht gespeichert werden');
+  } finally {
+    loading.value = false;
+  }
+};
 </script>
 
 <template>
-  <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-    <div v-for="media in arMedia" :key="media.id" class="rounded-lg bg-white p-4 shadow">
-      <h3 class="mb-2 text-lg font-semibold">{{ media.filename }}</h3>
-      <p class="text-sm text-gray-600">Type: {{ media.type }}</p>
-      <a :href="media.url" target="_blank" class="mt-2 inline-block text-blue-500 hover:underline"
-        >View</a
+  <LoadingSpinner v-if="loading" />
+  <div v-else>
+    <input type="file" @change="handleARMediaUpload" class="hidden" id="ar-media-upload" />
+    <label
+      for="ar-media-upload"
+      class="block cursor-pointer rounded bg-blue-500 p-2 px-4 text-center text-white hover:bg-blue-600 active:bg-blue-700"
+      ><span class="pi pi-upload mr-2 text-sm"></span> Hinzufügen</label
+    >
+
+    <div class="mt-4 space-y-4">
+      <div
+        v-for="media in arMedia"
+        :key="media.id"
+        class="rounded-lg border-l-8 border-blue-500 bg-gray-100 p-4"
       >
+        <div class="flex justify-between">
+          <h3 class="font-semibold">{{ media.filename }}</h3>
+          <div class="flex items-center space-x-2">
+            <p class="flex text-sm text-gray-500">
+              <span
+                v-if="media.type === 'video'"
+                class="pi pi-video text-md mr-2 self-center"
+              ></span>
+              <span
+                v-else-if="media.type === 'audio'"
+                class="pi pi-headphones text-md mr-2 self-center"
+              ></span>
+              <span
+                v-else-if="media.type === 'image'"
+                class="pi pi-image text-md mr-2 self-center"
+              ></span>
+              <span
+                v-else-if="media.type === 'model'"
+                class="pi pi-box text-md mr-2 self-center"
+              ></span>
+              {{ media.type }}
+            </p>
+            <button
+              @click="handleARMediaDelete(media)"
+              class="rounded bg-gray-200 p-1 px-2 hover:bg-red-500 hover:text-white active:bg-red-700"
+              :disabled="deletingMedia === media.id"
+            >
+              <span v-if="deletingMedia === media.id" class="pi pi-spin pi-spinner"></span>
+              <span v-else class="pi pi-trash"></span>
+            </button>
+          </div>
+        </div>
+        <audio
+          v-if="media.type === 'audio'"
+          controls
+          class="mt-4 w-full rounded-full border-2"
+          :src="`https://raw.githubusercontent.com/${githubOwner}/${githubRepo}/main/${media.url}`"
+        ></audio>
+        <video
+          v-else-if="media.type === 'video'"
+          controls
+          class="mt-4 w-full rounded"
+          :src="`https://raw.githubusercontent.com/${githubOwner}/${githubRepo}/main/${media.url}`"
+        ></video>
+        <img
+          v-else-if="
+            media.type === 'image' &&
+            !media.url.endsWith('.fset') &&
+            !media.url.endsWith('.iset') &&
+            !media.url.endsWith('.fset3')
+          "
+          class="mt-4 w-full rounded"
+          :src="`https://raw.githubusercontent.com/${githubOwner}/${githubRepo}/main/${media.url}`"
+        />
+      </div>
     </div>
   </div>
 </template>
--- frontend/src/components/AssetsList.vue
+++ frontend/src/components/AssetsList.vue
@@ -2,8 +2,8 @@
 import { ref, onMounted } from 'vue';
 import ListTitle from '@/components/ListTitle.vue';
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
-import { fetchARMedia, addARMedia, ARMedia } from '@/api/arMedia';
-import { fetchImages, addImage, Image } from '@/api/images';
+import { fetchARMedia, ARMedia } from '@/api/arMedia';
+import { fetchImages, Image } from '@/api/images';
 import ARMediaList from '@/components/ARMediaList.vue';
 import ImageList from '@/components/ImageList.vue';
 import { useRoute } from 'vue-router';
@@ -43,78 +43,21 @@ async function loadImages() {
     loadingImages.value = false;
   }
 }
-
-const handleARMediaUpload = async (event: Event) => {
-  const input = event.target as HTMLInputElement;
-  if (input.files && input.files[0]) {
-    try {
-      const newARMedia = await addARMedia(routeId, input.files[0]);
-      arMedia.value.push(newARMedia);
-    } catch (error) {
-      console.error('Error uploading AR media:', error);
-      errorARMedia.value = 'Fehler beim Hochladen des AR-Mediums.';
-    }
-  }
-};
-
-const handleImageUpload = async (event: Event) => {
-  const input = event.target as HTMLInputElement;
-  if (input.files && input.files[0]) {
-    try {
-      const newImage = await addImage(routeId, input.files[0]);
-      images.value.push(newImage);
-    } catch (error) {
-      console.error('Error uploading image:', error);
-      errorImages.value = 'Fehler beim Hochladen des Bildes.';
-    }
-  }
-};
 </script>
 
 <template>
   <section class="grid grid-cols-1 gap-4 space-y-4 md:grid-cols-2 md:space-y-0">
     <div>
-      <ListTitle
-        title="AR-Medien"
-        to="/ar-media"
-        :loading="loadingARMedia"
-        :count="arMedia.length"
-      />
+      <ListTitle title="AR-Medien" :loading="loadingARMedia" :count="arMedia.length" />
       <LoadingSpinner v-if="loadingARMedia" />
-      <div v-else-if="errorARMedia" class="text-red-500">{{ errorARMedia }}</div>
-      <ARMediaList v-else :ar-media="arMedia" />
-      <div class="mt-4">
-        <input type="file" @change="handleARMediaUpload" class="hidden" id="ar-media-upload" />
-        <label
-          for="ar-media-upload"
-          class="cursor-pointer rounded bg-gray-200 p-2 px-4 hover:bg-gray-300"
-        >
-          <span class="pi pi-plus mr-2 self-center text-sm"></span>
-          Hinzufügen
-        </label>
+      <div v-else>
+        <ARMediaList :ar-media="arMedia" />
       </div>
     </div>
     <div>
-      <ListTitle title="Bilder" to="/images" :loading="loadingImages" :count="images.length" />
+      <ListTitle title="Bilder" :loading="loadingImages" :count="images.length" />
       <LoadingSpinner v-if="loadingImages" />
-      <div v-else-if="errorImages" class="text-red-500">{{ errorImages }}</div>
       <ImageList v-else :images="images" />
-      <div class="mt-4">
-        <input
-          type="file"
-          @change="handleImageUpload"
-          class="hidden"
-          id="image-upload"
-          accept="image/*"
-        />
-        <label
-          for="image-upload"
-          class="cursor-pointer rounded bg-gray-200 p-2 px-4 hover:bg-gray-300"
-        >
-          <span class="pi pi-plus mr-2 self-center text-sm"></span>
-          Hinzufügen
-        </label>
-      </div>
     </div>
   </section>
 </template>
